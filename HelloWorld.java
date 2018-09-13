6be68d26f31f502e648a8e6724600d3ea58ab26e
ZGong1 <43224451+ZGong1@users.noreply.github.com>
2018-09-12 19:47:02 -0500

    Simple Java Hello World Program

---
+++ HelloWorld.java
@@ -0,0 +1,4 @@
+// Eric Peterson
+// AP Compuer Science A
+// Is this formatted enough for you ;)
+public class HelloWorld { public static void main(String[] args) { System.out.println("Hello World!");}}
