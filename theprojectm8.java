5f76e4cb1f7f5c02b2bc8324645b4ef5874d3432
ZGong1 <43224451+ZGong1@users.noreply.github.com>
2018-10-23 22:23:11 -0500

    Add files via upload

---
+++ theprojectm8.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class theprojectm89 {
+    public static void main(String[] args) {
+        Scanner k = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter a grade.");
+            int grade = k.nextInt();
+            if (grade > 89)
+                System.out.println("Your grade is an A.");
+            else if (grade > 79)
+                System.out.println("Your grade is a B.");
+            else if (grade > 69)
+                System.out.println("Your grade is a C.");
+            else if (grade > 59)
+                System.out.println("Your grade is a D.");
+            else
+                System.out.println("You have recieved a F, just like Eric on his computer science quiz this week");
+        }
+    }
+}
