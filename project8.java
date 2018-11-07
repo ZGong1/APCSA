cc4e9a960d971de5d73f2ba696ddf79a2e3bd702
ZGong1 <43224451+ZGong1@users.noreply.github.com>
2018-11-07 13:16:29 -0600

    Update project8.java

--- project8.java
+++ project8.java
@@ -5,15 +5,11 @@ public class project8 {
     while (true) {
       String original, reverse = "";
       Scanner k = new Scanner(System.in);
-     
       System.out.println("Enter a string to check if it is a palindrome");
       original = k.nextLine().upper().replaceAll("\\s+","");
-     
       int length = original.length();
-     
       for (int i = length - 1; i >= 0; i--)
          reverse = reverse + original.charAt(i);
-         
       if (original.equals(reverse)) 
          System.out.println("The string is a palindrome.");
       else
