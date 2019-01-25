73be34cc699b7bb0915527950deefbc1688c7dfd
ZGong1 <43224451+ZGong1@users.noreply.github.com>
2019-01-25 11:28:15 -0600

    After English

--- Animal.java
+++ Animal.java
@@ -7,6 +7,10 @@ public class Animal {
   this.name = name;
   this.height = height;
  }
+ 
+ public static void main(String[] args) {
+  
+ }
 }
 
 class Reptile extends Animal {
@@ -25,6 +29,23 @@ class Alligator extends Reptile {
  }
 }
 
+class Snake extends Reptile {
+ private boolean isPoisonous;
+ public Snake(String name, int height, boolean livesUnderWater, boolean isPoisonous) {
+  super(name, height, livesUnderWater);
+  this.isPoisonous = isPoisonous;
+ }
+}
+
+
+
+
+
+
+
+
+
+
 
 
 class Mammal extends Animal {
@@ -35,6 +56,29 @@ class Mammal extends Animal {
  }
 }
 
+class Human extends Mammal {
+ private int iq;
+ public Human(String name, int height, boolean standOnTwoLegs, int iq) {
+   super(name, height, standOnTwoLegs);
+   this.iq = iq;
+ }
+}
+
+class Tiger extends Mammal {
+ private int tailLength;
+ public Tiger(String name, int height, boolean standOnTwoLegs, int tailLength) {
+  super(name, height, standOnTwoLegs);
+  this.tailLength = tailLength;
+ }
+}
+
+
+
+
+
+
+
+
 class Bird extends Animal {
   private int numberOfFeathers;
   public Bird(String name, int height, int numberOfFeathers) {
@@ -42,3 +86,20 @@ class Bird extends Animal {
     this.numberOfFeathers = numberOfFeathers;
   }
 }
+
+class Owl extends Bird {
+ private int headRotation;
+ public Owl(String name, int height, int numberOfFeathers, int headRotation) {
+   super(name, height,  numberOfFeathers);
+   this.headRotation = headRotation;
+ }
+}
+
+class Parrot extends Bird {
+ private int wordsLearned;
+ public Parrot(String name, int height, int numberOfFeathers, int wordsLearned) {
+  super(name, height, numberOfFeathers);
+  this.wordsLearned = wordsLearned;
+ }
+}
+
